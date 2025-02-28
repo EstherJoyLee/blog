326520aacef7a2b148041916754714b741a2b91f
Esther <bonjourjj3@gmail.com>
2025-02-28 11:59:03 +0900

    modefied markdown's tailwind css

--- src/components/markdownRenderer/MarkdownRenderer.tsx
+++ src/components/markdownRenderer/MarkdownRenderer.tsx
@@ -17,33 +17,39 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
       components={{
         h1: ({ ...props }) => (
           <h1
-            className="text-3xl sm:text-2xl font-bold text-lime-500"
+            className="text-3xl sm:text-2xl font-bold text-lime-500 my-3"
             {...props}
           />
         ),
         h2: ({ ...props }) => (
           <h2
-            className="text-2xl sm:text-xl font-semibold text-blue-500"
+            className="text-2xl sm:text-xl font-semibold text-blue-500 my-3"
             {...props}
           />
         ),
         h3: ({ ...props }) => (
-          <h3 className="text-xl font-semibold text-cyan-500" {...props} />
+          <h3 className="text-xl font-semibold text-cyan-500 my-3" {...props} />
         ),
         h4: ({ ...props }) => (
-          <h4 className="text-xl font-semibold text-fuchsia-500" {...props} />
+          <h4
+            className="text-xl font-semibold text-fuchsia-500 my-3"
+            {...props}
+          />
         ),
         h5: ({ ...props }) => (
-          <h5 className="text-xl font-semibold text-rose-500" {...props} />
+          <h5 className="text-xl font-semibold text-rose-500 my-3" {...props} />
         ),
         h6: ({ ...props }) => (
-          <h6 className="text-xl font-semibold text-slate-500" {...props} />
+          <h6
+            className="text-xl font-semibold text-slate-500 my-3"
+            {...props}
+          />
         ),
         p: ({ ...props }) => (
-          <p className="text-base leading-relaxed" {...props} />
+          <p className="text-base leading-relaxed my-3" {...props} />
         ),
         strong: ({ ...props }) => <strong className="font-bold" {...props} />,
-        ul: ({ ...props }) => <ul className="list-disc ml-5" {...props} />,
+        ul: ({ ...props }) => <ul className="list-disc ml-5 my-3" {...props} />,
         ol: ({ ...props }) => <ol className="list-decimal ml-5" {...props} />,
         li: ({ ...props }) => <li className="mb-2" {...props} />,
         span: ({ ...props }) => <span {...props} />,
@@ -57,7 +63,7 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
         ),
         pre: ({ ...props }) => (
           <pre
-            className="text-gray-100 p-3 rounded-lg overflow-x-auto"
+            className="text-gray-100 p-3 rounded-lg overflow-x-auto my-5"
             {...props}
           />
         ),
